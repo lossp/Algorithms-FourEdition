ad2a2a3d58697a5a321d0a92a23c1297dec6c2f6
lossp <richard.xxmxx@gmail.com>
2019-01-08 10:04:31 +0800

    update the comments about InfixToPrefix

--- chapter-1.3/InfixToPrefix.java
+++ chapter-1.3/InfixToPrefix.java
@@ -1,4 +1,9 @@
-import edu.princeton.cs.algs4;
+import edu.princeton.cs.algs4.Stack;
+/**
+*  Here is only the execrise aboout prefix and infix.
+*  Computer usually use infix expression to calculate.
+*  Here is basically i know as a new beginner.
+*/
 public class PrefixIntoInfix
 {
 	public static void main(String[] args)
@@ -17,72 +22,80 @@ public class PrefixIntoInfix
 		}
 		// now pop out the character from reverseStack. now we got the character of infixExpress in a reverse way
 		while(!reverseStack.isEmpty())
-		{
+		{	
+			// get one character out of the reverseStack
 			String str = reverseStack.pop();
+
+			// if the str equals ")", then push it into the operator stack
 			if (str.equals(")"))
 			{
 				operators.push(str);
 			}
-			else if (str.equals("+") || str.equals("-") || str.equals("*") || str.equals("/"))
+			/** 
+			*   if the str is one of the "+ - * /", keeping looping under the four conditions below
+			*      1.if the stack is empty, then push it into the stack and end this step.
+			*      2.if the top of stack is ")", then push it into the stack and end this step.
+			*      3.if this operator at least have the weight as the top stack element, then push it into the stack and end this step.
+			*      4.otherwise, operator keeps poping out of the stack operators, util meets the three condition above, then push it into the stack.
+			*/      
+			else if (str.equals("+") || str.equals('-') || str.equals("*") || str.equals("/"))
 			{
 				boolean isRightParen;
-				boolean isEmpty;
-				boolean isWeighterOperater;
+				boolean isWeighterOperator;
 				int i = 0;
-				do 
+				do
 				{
 					if (i != 0)
 					{
 						values.push(operators.pop());
 					}
-					i ++;
+					i++;
 					isRightParen = false;
-					isWeighterOperater = false;
+					isWeighterOperator =false
 					if (!operators.isEmpty())
 					{
 						String stackTop = operators.peek();
 						isRightParen = stackTop.equals(")");
 						if (stackTop.equals("+") || stackTop.equals("-"))
 						{
-							isWeighterOperater = true;
+							isWeighterOperator = true;
 						}
-						else if ((stackTop.equals("*") || stackTop.equals("/"))
-                                && (str.equals("*") || str.equals("/")))
+						else if ((stackTop.equals("*") || stackTop.equals("/")) && (str.equals("*") || str.equals("/")))
 						{
-							isWeighterOperater = true;
+							isWeighterOperator = true;
 						}
 					}
-					if (operators.isEmpty() || isRightParen || isWeighterOperater)
+					if (operators.isEmpty() || isRightParen || isWeighterOperator)
 					{
 						operators.push(str);
 						break;
 					}
-				}
-                while(!(isEmpty || isRightParen || isHigherOperator))
-
+				} while (!(isEmpty || isRightParen || isWeighterOperator))
 			}
-			else if (str.equals("("))
+
+			// if the str equals "(", then pop out the charactor out of the operators until it meets the ")", and toss the ")"
+			else if (str.equals(")"))
 			{
 				String poppedString = operators.pop();
-				while (!poppedString.equals(")"))
+				while(!poppedString.equals(")"))
 				{
 					values.push(poppedString);
 					poppedString = operators.pop();
 				}
 			}
-			else 
+			else
 			{
 				values.push(str);
 			}
-			
 		}
-        for (String str:operators)
-        {
-            values.push(str);
-        }
-        for (String str:values)
-        {
-            System.out.print(str);
-        }
+		for (String str:operators)
+		{
+			values.push(str);
+		}
+		for (String str: values)
+		{
+			System.out.print(str);
+		}
+        
 	}
 }
